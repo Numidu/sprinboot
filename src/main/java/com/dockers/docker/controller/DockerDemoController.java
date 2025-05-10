0372ff3d742402c0216fac1b01738b753189e3f7
Numidu <dnumidu@gmail.com>
2025-05-10 23:27:50 +0530

    change return name

--- src/main/java/com/dockers/docker/controller/DockerDemoController.java
+++ src/main/java/com/dockers/docker/controller/DockerDemoController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DockerDemoController {
     @GetMapping("testAPI")
     public  String testMethod(){
-        return "I am working fine";
+        return "I am working at university";
     }
 }
